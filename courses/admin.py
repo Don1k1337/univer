c079dfca3f407d452bf7f1262b19e76ceb340200
Don1k1337 <dan4ik1411@gmail.com>
2020-04-28 16:16:45 +0600

    Django Admin Customization - filtering, searching

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from . models import *
+
+# Action func
+def change_actuality(modeladmin, request, queryset):
+    queryset.update(actuality = 'a')
+
+# Action description
+change_actuality.short_description = "Mark Selected Courses As Actual"
+
+    
+
 # Register your models here.
-admin.site.register(Course)
 admin.site.register(Major)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'language', 'abbreviation', 'ccode', 'major')
+    list_filter = ('title',)
+    search_fields = ('title', 'abbreviation')
+    ordering = ['title', 'abbreviation']
+    actions = [change_actuality]
+
+# Main Register for Models
+admin.site.register(Course, CourseAdmin)
+
+# Changing Admin header
+admin.site.site_header = "IUCA Course System"
+admin.site.unregister(Group)
+
--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from students.models import  Major
 
+Actuality = [
+    ('n', 'New'),
+    ('a', 'Added'),
+    ('s', 'Skipped')
+]
+
 class Course(models.Model):
     title = models.CharField(max_length=40, default='')
     description = models.CharField(max_length=60)
@@ -8,7 +14,10 @@ class Course(models.Model):
     abbreviation = models.CharField(max_length=40, default='')
     ccode = models.CharField(max_length=4)
     major = models.ForeignKey(Major, on_delete=models.CASCADE)
+    actuality = models.CharField(max_length = 1, choices = Actuality, default='')
 
     def __str__(self):
         return self.abbreviation
+    def show_desc(self):
+        return self.description[:50]
 
